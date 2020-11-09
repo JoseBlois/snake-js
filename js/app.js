a3a2a88856307733e92246531e7da8000552abbf
jose blois <joseblois3@gmail.com>
2020-11-09 16:50:33 -0300

    defined keys values added a repaint function

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
 var canvas = null;
 var ctx  = null;
 var x = 50, y = 50;
+var lastPress = null;
+var KEY_LEFT = 37,
+    KEY_UP = 38,
+    KEY_RIGHT = 39,
+    KEY_DOWN = 40;
+var dir = 0;
+window.requestAnimationFrame = (function () {
+    return window.requestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        function (callback) {
+            window.setTimeout(callback, 17);
+        };
+}());
+
+document.addEventListener('keydown',function(evt){
+    lastPress = evt.which;
+},false);
 
 function paint(ctx){
     ctx.fillStyle = '#000';
@@ -8,6 +26,8 @@ function paint(ctx){
 
     ctx.fillStyle = '#0f0';
     ctx.fillRect(x,y,10,10);
+
+    ctx.fillText('Last Press: ' + lastPress, 0, 20);
 }
 
 function act(){
@@ -17,25 +37,22 @@ function act(){
     }
 }
 
-function run(){
-    window.requestAnimationFrame(run);
-    act();
+function repaint(){
+    window.requestAnimationFrame(repaint);
     paint(ctx);
+}
+
+function run(){
+    setTimeout(run, 50);
+    act();
 }
 
 function init(){
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-    run()
+    run();
+    repaint();
 }
 
 window.addEventListener('load',init,false);
 
-Window.requestAnimationFrame = (function () {
-    return window.requestAnimationFrame ||
-        window.mozRequestAnimationFrame ||
-        window.webkitRequestAnimationFrame ||
-        function (callback) {
-            window.setTimeout(callback, 17);
-        };
-}());
