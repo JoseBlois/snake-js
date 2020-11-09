d42d1fd671d79959aa35e124e0bfd62e125efcb8
jose blois <joseblois3@gmail.com>
2020-11-09 17:18:31 -0300

    Pause button and directions done working on a rectangle object

--- js/app.js
+++ js/app.js
@@ -5,8 +5,10 @@ var lastPress = null;
 var KEY_LEFT = 37,
     KEY_UP = 38,
     KEY_RIGHT = 39,
-    KEY_DOWN = 40;
+    KEY_DOWN = 40,
+    KEY_ENTER = 13;
 var dir = 0;
+var pause = true;
 window.requestAnimationFrame = (function () {
     return window.requestAnimationFrame ||
         window.mozRequestAnimationFrame ||
@@ -20,6 +22,17 @@ document.addEventListener('keydown',function(evt){
     lastPress = evt.which;
 },false);
 
+function Rectangle (x ,y , width, height){
+    this.x = (x == null) ? 0 : x;
+    this.y = (y == null) ? 0 : y;
+    this.width = (width == null) ? 0 : width;
+    this.height = (height == null) ? this.width : height;
+
+    this.intersect = function(rect){
+
+    }
+}
+
 function paint(ctx){
     ctx.fillStyle = '#000';
     ctx.fillRect(0,0,canvas.width,canvas.height);
@@ -27,13 +40,63 @@ function paint(ctx){
     ctx.fillStyle = '#0f0';
     ctx.fillRect(x,y,10,10);
 
+    ctx.fillStyle = '#fff'
     ctx.fillText('Last Press: ' + lastPress, 0, 20);
+
+    if(pause){
+        ctx.textAlign = 'center';
+        ctx.fillText('PAUSE', 150, 75);
+        ctx.textAlign = 'left';
+    }
 }
 
 function act(){
-    x += 2;
-    if(x > canvas.width){
-        x = 0;
+    if(!pause){
+        //Change Direction
+        if(lastPress === KEY_UP) {
+            dir = 0;
+        }
+        if(lastPress === KEY_RIGHT) {
+            dir = 1;
+        }
+        if(lastPress === KEY_DOWN) {
+            dir = 2;
+        }
+        if(lastPress === KEY_LEFT) {
+            dir = 3;
+        }
+
+        //Move Rect
+        if(dir === 0){
+            y -= 10;
+        }
+        if(dir === 1){
+            x += 10;
+        }
+        if(dir === 2){
+            y += 10;
+        }
+        if(dir === 3){
+            x -= 10;
+        }
+
+        //Out Screen
+        if(x >= canvas.width){
+            x = 0;
+        }
+        if(y >= canvas.height){
+            y = 0;
+        }
+        if(x < 0){
+            x = canvas.width;
+        }
+        if(y < 0){
+            y = canvas.height;
+        }
+    }
+    if(lastPress === KEY_ENTER){
+        pause = !pause;
+        lastPress = null;
     }
 }
 
